e16d2a216e060ae154f4ee924ffc1e4aa366dc27
Raphael-GC <raphaelgc.i9@gmail.com>
2021-08-18 08:12:05 -0300

    Ch1 - Propriedades no React

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,9 +1,9 @@
-export function Repository() {
+export function RepositoryItem(props) {
     return (
         <li>
-            <strong>unform</strong>
-            <p>Forms in React</p>
-            <a href="">
+            <strong>{props.repository.name ?? 'Default'}</strong>
+            <p>{props.repository.description}</p>
+            <a href={props.repository.link}>
                 Acessar repositorios
             </a>
         </li>
